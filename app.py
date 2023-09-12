ea9c5cdeaf316f7d69a19829b9102e7cb2071a60
correiasj <meugato556@gmail.com>
2023-09-12 15:59:27 -0300

    1

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
 
-@app.route('/')
-def hello_world():
-    return 'teste'
+app = Flask(__name__)
+# route -> hashtagtreinamentos.com/
+# função -> o que você quer exibir naquela página
+# template
+
+@app.route("/")
+def home():
+    return render_template("home.html")
